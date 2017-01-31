91a843afaba6050aa645a8933cb34c156d89f4fc
vezuniga <victor.e.zuniga@qvc.com>
2017-01-31 14:51:25 -0500

    Lab work 7.2

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,3 +44,8 @@ node default {
   #   class { 'my_class': }
   include role::classroom
 }
+
+exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+   path => '/usr/bin:/usr/local/bin',
+   creates => '/etc/motd',
+}
