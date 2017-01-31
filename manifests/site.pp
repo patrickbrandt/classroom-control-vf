7411276d97983ab55cb2f4319b619c1f11b79e56
Marcellus Easley <marcellus.easley@gmail.com>
2017-01-31 14:49:25 -0500

    added motd to be friendly
    added motd to be friendly

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,8 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  file { '/etc/motd':
+  ensure => file,
+  content => "Today I learned how to puppetize all the things!"
+  }
 }
