1ab5a85f3e822dcc6e3dabf99b6a87cc78635609
Drew Nase <dnasecs@gmail.com>
2017-01-31 14:48:02 -0500

    learning how to moar puppetize

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,7 +45,7 @@ node default {
   include role::classroom
   notify { 'you have changed stuff': }
   file { '/etc/motd':
-    ensure => file,
+    ensure  => file,
     content => 'Today I learned how to puppetize all the things!',
   }
 }
