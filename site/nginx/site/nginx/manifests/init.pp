b3eedf34b9b4277560eacf1e3330f541602b9f7b
Antonio Sotelo <asotelo6@gmail.com>
2017-02-01 12:54:10 -0600

    managing nginx

--- site/nginx/site/nginx/manifests/init.pp
+++ site/nginx/site/nginx/manifests/init.pp
@@ -1,14 +1,44 @@
 class nginx {
-  package {
-  
+  package { 'nginx':
+    ensure => latest,
   }
-  file {
-  
+  file { 'conf.d':
+    ensure => directory,
+    path => '/etc/nginx/conf.d',
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
   }
-  
-  service {
-  
+  file { 'default.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    path => '/etc/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
   }
-  
-
+  file { 'nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+    path => '/etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+  file { 'docroot':
+    ensure => directory,
+    path => '/var/www',
+  }
+  file { 'index.html':
+    ensure => file,
+    path => '/var/www/index.html',
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => File['nginx.conf', 'default.conf'],
+  }
 }
