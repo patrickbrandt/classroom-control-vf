cfb52217299563e86b18078475ab7957ff104da7
Madhusudhan Devireddy <mdreddy@intercall.com>
2017-02-01 15:13:04 -0500

    Update site.pp
    conditional exercise

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,21 +38,29 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+lebucket by default for all File resources:File { backup => false }
+# Randomize enforcement order to help understand relationships
+ini_setting { 'random ordering':
+  ensure  => present,
+  path    => "${settings::confdir}/puppet.conf",
+  section => 'agent',
+  setting => 'ordering',
+  value   => 'title-hash',
+}
+# DEFAULT NODE
+# Node definitions in this file are merged with node data from the console. See
+# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.
+# The default node definition matches any node lacking a more specific node
+# definition. If there are no other nodes in this file, classes declared here
+# will be included in every node's catalog, *in addition* to any classes
+# specified in the console for that node.
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  include role::classroom
- 
-  
-  notify{'you have changed stuff':}
-  file{'/etc/motd':
-  ensure => file,
-  
-  }
-  exec {'motd cmd':
-  command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
-  creates => '/etc/motd',
-  path =>'user/local/bin',
+  if $::virtual != 'physical' {
+     $vmname = capitalize($::virtual)
+    notify { "This is a ${vmname} virtual machine.": }
   }
 }
