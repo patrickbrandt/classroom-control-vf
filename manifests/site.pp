b6bb4f0b79020291e15030d49abe1ed752c6d3fa
mczepieltpc <mczepieltpc@gmail.com>
2017-02-01 10:22:08 -0500

    include skeleton

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,6 +44,7 @@ node default {
   #   class { 'my_class': }
   include role::classroom
   include users
+  include skeleton
   
  #exec { 'createfile':
  #   command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
