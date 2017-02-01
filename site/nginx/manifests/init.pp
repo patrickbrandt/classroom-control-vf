2d11e3ab4b23f1433592846acca8ec4f6d36d13a
iadiadiad <iadiadiad@gmail.com>
2017-02-01 13:51:51 -0500

    nginx

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,42 @@
+class nginx {
+package { 'nginx':
+  ensure => latest
+}
+file { 'conf.d':
+  ensure => directory,
+  path => '/etc/nginx/conf.d',
+  owner => 'root',
+  group => 'root',
+  mode => '0775',
+  }
+file { 'default.conf':
+  ensure => file,
+  owner => 'root',
+  group => 'root',
+  mode => '0664',
+  path => /etc/nginx/conf.d/default.conf',
+  source => 'puppet:///modules/nginx/default.conf',
+  require => Package['nginx'],
+}
+file { 'nginx.conf':
+  ensure => file,
+  path => /etc/nginx/conf.d/nginx.conf',
+  source => 'puppet:///modules/nginx/nginx.conf',
+  require => Package['nginx'],
+}
+file {'docroot':
+  ensure => direcroty,
+  path => '/var/www',
+}
+file {/index.html':
+  ensure => file,
+  path => /etc/nginx/conf.d/index.conf',
+  source => 'puppet:///modules/nginx/index.conf',
+  require => Package['nginx'],
+}
+service {'nginx'
+  ensure => running,
+  enable => true,
+  subscribe => File['nginx.conf', default.conf'],
+}
+}
