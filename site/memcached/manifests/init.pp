5dfad2e73172df91491b3c91e1bfc00a5062d139
ragnarkon <ragnarkon@gmail.com>
2017-02-01 13:32:26 -0500

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,16 +1,17 @@
 class memcached {
   package{ 'memcached':
-    present => yes, #wrong
+    ensure => latest, #wrong
   }
   
   file{ '/etc/sysconfig/memcached':
     ensure => present,
-    source => ,
+    source => 'puppet:///modules/memcached/memcached,
     requires => Package['memcached'],
   }
   
   service{ 'memcached':
     ensure => running,
+    enable => true,
     requires => File['/etc/sysconfig/memcached'],
   }
 }
