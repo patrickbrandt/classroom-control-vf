b09f5e5f9c1e8bec95ff7e3689688891626acc04
Madhusudhan Devireddy <mdreddy@intercall.com>
2017-02-01 13:15:15 -0500

    Update init.pp
    fix

--- site/memcached/manifest/init.pp
+++ site/memcached/manifest/init.pp
@@ -1,18 +1,1 @@
-class memcached {
-package {'memcached':
-  ensure => latest,
-}
-
-file {'memcached.conf'
-ensure => file,
-path => '/etc/sysconfig/memcached'
-source => 'puppet:///modules/memcached/memcached',
-require => Package ['memchached'],
-}
-
-service {'memcached':
-ensure => running,
-enable => true,
-subscribe => File['memcached.conf']
-}
-}
+class memcached {  package { 'memcached':    ensure => latest,  }  file { 'memcached.conf':    ensure => file,    path => '/etc/sysconfig/memcached',    source => 'puppet:///modules/memcached/memcached',    require => Package['memcached'],  }  service { 'memcached':    ensure => running,    enable => true,    subscribe => File['memcached.conf']  }}
