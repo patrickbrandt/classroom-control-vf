b01a28db0260b16abddf4a9b1549962a1ff2ae3c
iadiadiad <iadiadiad@gmail.com>
2017-02-01 13:53:53 -0500

    typos...

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -14,7 +14,7 @@ file { 'default.conf':
   owner => 'root',
   group => 'root',
   mode => '0664',
-  path => /etc/nginx/conf.d/default.conf',
+  path => '/etc/nginx/conf.d/default.conf',
   source => 'puppet:///modules/nginx/default.conf',
   require => Package['nginx'],
 }
@@ -25,18 +25,18 @@ file { 'nginx.conf':
   require => Package['nginx'],
 }
 file {'docroot':
-  ensure => direcroty,
+  ensure => directory,
   path => '/var/www',
 }
 file {/index.html':
   ensure => file,
-  path => /etc/nginx/conf.d/index.conf',
+  path => '/etc/nginx/conf.d/index.conf',
   source => 'puppet:///modules/nginx/index.conf',
   require => Package['nginx'],
 }
 service {'nginx'
   ensure => running,
   enable => true,
-  subscribe => File['nginx.conf', default.conf'],
+  subscribe => File['nginx.conf', 'default.conf'],
 }
 }
