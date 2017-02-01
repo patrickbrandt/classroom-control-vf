c04def2e61dc83bb0410f8900466499e99a5f056
Patrick Brandt <pbrandt@coca-cola.com>
2017-02-01 14:50:37 -0500

    refactoring - excercise 13.1

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,36 @@
 class nginx {
+	File {
+		owner	=> 'root',
+		group   => 'root',
+		mode    => '0664',
+	}
+
+	$nginx_conf = '/etc/nginx/nginx.conf'
+	$default_conf = '/etc/nginx/conf.d/default.conf'
+	
 	package { 'nginx':
 		ensure => latest,
 	}
 	file { '/etc/nginx/conf.d':
 		ensure  => directory,
-		owner	=> 'root',
-		group   => 'root',
-		mode    => '0775',
 	}
 	file { '/var/www':
 		ensure  => directory,
-		owner	=> 'root',
-		group   => 'root',
-		mode    => '0775',
 	}
 	service { 'nginx':
 		enable      => true,
 		ensure      => running,
-		subscribe	=> File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/default.conf']
+		subscribe	=> File[$nginx_conf, $default_conf]
 	}
-	file { '/etc/nginx/nginx.conf':
+	file { $nginx_conf:
 		ensure	=> file,
 		source	=> 'puppet:///modules/nginx/nginx.conf',
 		require => Package['nginx'],
-		owner	=> 'root',
-		group   => 'root',
-		mode    => '0664',
 	}
-	file { '/etc/nginx/conf.d/default.conf':
+	file { $default_conf:
 		ensure	=> file,
 		source	=> 'puppet:///modules/nginx/default.conf',
 		require => Package['nginx'],
-		owner	=> 'root',
-		group   => 'root',
-		mode    => '0664',
 	}
 	file { '/var/www/index.html': 
 		ensure	=> file,
