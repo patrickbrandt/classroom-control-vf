281f290f3235819997c3310b2727578ae0af4b5c
vezuniga <victor.e.zuniga@qvc.com>
2017-02-01 10:19:34 -0500

    lab 9.3 - skeleton class

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,13 @@
+class skeleton { 
+   file {'/etc/skel':
+      ensure => directory,
+      }
+      
+   file {'/etc/skel/.bashrc':
+      ensure => file,
+      owner => 'root',
+      group => 'root',
+      mode => '0644',
+      source => 'puppet:///modules/skeleton/bashrc',
+      }
+}
