4919d5d679c0c2fb35a22af3281961d6eb13ac6f
ragnarkon <ragnarkon@gmail.com>
2017-02-01 13:46:51 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -7,26 +7,35 @@ class nginx {
   file { 'nginx.conf':
     ensure => file,
     path => '/etc/nginx/nginx.conf',
-    source => 'puppet:///modules/nginx/default.conf',
-    requires => Package['nginx'],
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
   }
   
   file { 'default.conf':
     ensure => file,
     path => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
-    requires => Package['nginx'],
+    require => Package['nginx'],
   }
   
   file { 'docroot':
-    ensure => folder,
+    ensure => directory,
     path => '/var/www',
-    requires => Package['nginx'],
+    require => Package['nginx'],
+  }
+  
+  file { 'index.html':
+    ensure => file,
+    path => '/var/www/index.html',
+    source => 'puppet:///modules/nginx/index.html'
+    require => File['docroot'],
   }
   
   service { 'nginx':
     ensure => running,
     enable => true,
-    requires => Package['nginx'],
+    require => Package['nginx'],
+    subscribe => File['nginx,conf', 'default.conf'],
   }
+  
 }
