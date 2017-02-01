e3c9d1443ca0a0045ae45f9bfc4ee51d251d2f4c
Patrick Brandt <pbrandt@coca-cola.com>
2017-02-01 10:21:23 -0500

    excercise 9.3

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,14 @@
+class skeleton {
+  file { '/etc/skel':
+    ensure => directory,
+  }
+  
+  file{'/etc/skel/.bashrc':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode  => '0440',
+    source  => 'puppet:///modules/skelton/bashrc',
+  
+  }
+}
