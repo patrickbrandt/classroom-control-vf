dfd1bbac07e39dc3158fb175be5b3c077b7bf821
Patrick Brandt <pbrandt@coca-cola.com>
2017-02-01 13:09:35 -0500

    memcached config - lesson 11.1

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,16 @@
+class memcached {
+	package { 'memcached':
+		ensure => latest,
+	}
+	service { 'memcached':
+		enable      => true,
+		ensure      => running,
+		subscribe => File['/etc/sysconfig/memcached'],
+	}
+	file { '/etc/sysconfig/memcached':
+		ensure  => file,
+		source	=> 'puppet:///modules/memcached/config',
+		require  => Package['memcached'],
+
+	}
+}
