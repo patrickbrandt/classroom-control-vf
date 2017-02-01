9fb5d669959e687cd8eca7917c5100429bd06933
Patrick Brandt <pbrandt@coca-cola.com>
2017-02-01 10:08:19 -0500

    excercises 9.1 and 9.2

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,6 +43,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  include users
   exec {'motd cmd':
     creates   => '/etc/motd',
     path      => '/usr/local/bin',
