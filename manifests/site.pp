15ac015f5f8fe5f0f65c9886c2d164582cd7aac9
Madhusudhan Devireddy <mdreddy@intercall.com>
2017-02-02 11:09:21 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -56,8 +56,58 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
+  nces.
+# Disable filebucket by default for all File resources:
+File { backup => false }
+# Randomize enforcement order to help understand relationships
+ini_setting { 'random ordering':
+  ensure  => present,
+  path    => "${settings::confdir}/puppet.conf",
+  section => 'agent',
+  setting => 'ordering',
+  value   => 'title-hash',
+}
+# DEFAULT NODE
+# Node definitions in this file are merged with node data from the console. See
+# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.
+# The default node definition matches any node lacking a more specific node
+# definition. If there are no other nodes in this file, classes declared here
+# will be included in every node's catalog, *in addition* to any classes
+# specified in the console for that node.
+#lebucket by default for all File resources:File { backup => false }
+# Randomize enforcement order to help understand relationships
+ini_setting { 'random ordering':
+  ensure  => present,
+  path    => "${settings::confdir}/puppet.conf",
+  section => 'agent',
+  setting => 'ordering',
+  value   => 'title-hash',
+}
+# DEFAULT NODE
+# Node definitions in this file are merged with node data from the console. See
+# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.
+# The default node definition matches any node lacking a more specific node
+# definition. If there are no other nodes in this file, classes declared here
+# will be included in every node's catalog, *in addition* to any classes
+# specified in the console for that node.
+node default {
   
+  # This is where you can declare classes for all nodes.    
+  # Example:   
+  #   class { 'my_class': }    
+  include role::classroom    
+  include skeleton    
+  exec {'motd cmd':      
+  command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",      
+  creates => '/etc/motd',      
+  path => '/usr/local/bin',    }    
+  host { 'testing.puppetlabs.vm':      
+  ensure       => 'present',      
+  host_aliases => ['testing'],      
+  ip           => '127.0.0.1',    }    
+  if $::virtual != 'physical' {      
+  $vmname = capitalize($::virtual)      
+  notify { "This machine is using ${vmname}": }  } 
 }
