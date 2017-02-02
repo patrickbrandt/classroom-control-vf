4d9bd9860ad5b1a0393c42a284bae3f182a395cb
reggie2311 <reggie2311@users.noreply.github.com>
2017-02-02 08:38:19 -0500

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,40 @@
+class nginx {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode => '0664',
+  }
+  package { 'nginx':
+    ensure => latest,
+  }
+  file { 'conf.d':
+    ensure => directory,
+    path => '/etc/nginx/conf.d',
+  }
+  file { 'default.conf':
+    ensure => file,
+    path => '/etc/nginx/conf.d/default.conf',
+    source => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+  file { 'nginx.conf':
+    ensure => file,
+    path => '/etc/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+  file { 'docroot':
+    ensure => directory,
+    path => '/var/www',
+  }
+  file { 'index.html':
+    ensure => file,
+    path => '/var/www/index.html',
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+    subscribe => File['nginx.conf', 'default.conf'],
+  }
+}
