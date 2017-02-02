2d41cf6da6dd24c1716798b165aa65329b0c6ac8
Drew Nase <dnasecs@gmail.com>
2017-02-02 09:16:07 -0500

    create defined type to manage user

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,5 +1,16 @@
 define users::managed_user (
-
+  $username = $title,
+  $group = $title,
 ) {
-
+  user { $username:
+    ensure => present,
+  }
+  file { "/home/${username}":
+    ensure => directory,
+    owner => $username,
+    group => $group,
+  }
+  group { $group:
+    ensure => present,
+  }
 }
